c62ee53c6ea308a9d725ad0fea17e1953a11d391
aribilgiogretmen <aribilgiogretmen@gmail.com>
2024-05-26 14:25:02 +0300

    Hr Eklendi

---
+++ Ders1-MvcTemelleri/Controllers/HRController.cs
@@ -0,0 +1,22 @@
+﻿using Ders1_MvcTemelleri.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ders1_MvcTemelleri.Controllers
+{
+    public class HRController : Controller
+    {
+        private static List<HR> hr = new List<HR>
+        {
+
+            new HR{FirstName="Melike", lastName="Şahin",Age=30},
+            new HR{FirstName="Serdar", lastName="Ortaç",Age=55},
+            new HR{FirstName="Sertap", lastName="Erener",Age=60}
+        };
+
+        [HttpGet("/Hr")]
+        public IActionResult Index()
+        {
+            return View(hr);
+        }
+    }
+}
---
+++ Ders1-MvcTemelleri/Models/HR.cs
@@ -0,0 +1,9 @@
+﻿namespace Ders1_MvcTemelleri.Models
+{
+    public class HR
+    {
+        public string FirstName { get; set; }
+        public string lastName { get; set; }
+        public int Age { get; set; }
+    }
+}
